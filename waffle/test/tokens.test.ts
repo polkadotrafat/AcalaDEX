aca836cb565b41709f9da819133118c6712aff20
Rafat Hussain <rafat.hsn@gmail.com>
2021-02-11 05:27:38 +0530

    trade test 6

--- waffle/test/tokens.test.ts
+++ waffle/test/tokens.test.ts
@@ -214,7 +214,7 @@ describe("Tokens", () => {
     let offers;
 
     if (parseInt(offerSize.toString()) > 0) {
-        console.log("Order Book");
+        console.log("ACA/DOT Order Book");
         offers = await Exchange.getOfferPerId(offerId);
         console.log(offerId.toString(),offers[0],offers[1].toString(),offers[2],offers[3].toString());
 
@@ -286,7 +286,49 @@ describe("Tokens", () => {
           offers = await Exchange2.getOfferPerId(offerId);
           console.log(offerId.toString(),offers[0],offers[1].toString(),offers[2],offers[3].toString());
       }
-  }
+    }
+
+    // Manually Cancel all the outstanding orders
+
+    //ACA-DOT Trade
+
+    offerSize = await Exchange.getOfferSize(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
+
+    if (parseInt(offerSize.toString()) > 0) {
+      offerId = await Exchange.getBestOffer(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
+      let success = await Exchange.cancelOffer(offerId);
+      for(let i = 1; i < parseInt(offerSize.toString()); ++i) {
+        offerId = await Exchange.getPrevOffer(offerId);
+        success = await Exchange.cancelOffer(offerId);
+      }
+    }
+
+    offerSize = await Exchange2.getOfferSize(DOT_ERC20_ADDRESS,ACA_ERC20_ADDRESS);
+
+    if (parseInt(offerSize.toString()) > 0) {
+      offerId = await Exchange2.getBestOffer(DOT_ERC20_ADDRESS,ACA_ERC20_ADDRESS);
+      let success = await Exchange2.cancelOffer(offerId);
+      for(let i = 1; i < parseInt(offerSize.toString()); ++i) {
+        offerId = await Exchange2.getPrevOffer(offerId);
+        success = await Exchange2.cancelOffer(offerId);
+      }
+    }
+
+    //Check that all orders are cancelled
+
+    offerSize = await Exchange.getOfferSize(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
+
+    offerId = await Exchange.getBestOffer(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
+
+    expect(offerSize.toString(),"0");
+    expect(offerId.toString(), "0");
+
+    offerSize = await Exchange2.getOfferSize(DOT_ERC20_ADDRESS,ACA_ERC20_ADDRESS);
+
+    offerId = await Exchange2.getBestOffer(DOT_ERC20_ADDRESS,ACA_ERC20_ADDRESS);
+
+    expect(offerSize.toString(),"1");
+    expect(offerId.toString(), "4");
 
   });
 });
