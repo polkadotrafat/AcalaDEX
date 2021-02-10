eb61e551faa6e8193bdea7815386e6f82cd04c6e
Rafat Hussain <rafat.hsn@gmail.com>
2021-02-10 06:11:51 +0530

    initial balances

--- waffle/test/tokens.test.ts
+++ waffle/test/tokens.test.ts
@@ -11,9 +11,12 @@ import DEX from "../build/Dex.json";
 use(solidity);
 use(evmChai);
 
-const DOT_ERC20_ADDRESS = '0x0000000000000000000000000000000000000802';
-
 const ACA_ERC20_ADDRESS = '0x0000000000000000000000000000000000000800';
+const AUSD_ERC20_ADDRESS = '0x0000000000000000000000000000000000000801';
+const DOT_ERC20_ADDRESS = '0x0000000000000000000000000000000000000802';
+const XBTC_ERC20_ADDRESS = '0x0000000000000000000000000000000000000803';
+const LDOT_ERC20_ADDRESS = '0x0000000000000000000000000000000000000804';
+const RENBTC_ERC20_ADDRESS = '0x0000000000000000000000000000000000000805';
 
 const provider = new Provider({
   provider: new WsProvider("ws://127.0.0.1:9944"),
@@ -52,27 +55,53 @@ const getWallets = async () => {
 };
 
 describe("Tokens", () => {
-  let wallet: Signer;
-  let walletTo: Signer;
+  let wallet1: Signer;
+  let wallet2: Signer;
   let DOT: Contract;
+  let ACA: Contract;
+  let AUSD: Contract;
+  let XBTC: Cotract;
+  let LDOT: Contract;
+  let RENBTC: Contract;
   
   before(async () => {
-    [wallet, walletTo] = await getWallets();
-    //console.log(wallet);
-    //console.log(DEX);
-    //console.log(IERC20);
-    DOT = new ethers.Contract(DOT_ERC20_ADDRESS, IERC20.abi, wallet);
+    [wallet1, wallet2] = await getWallets();
+    DOT = new ethers.Contract(DOT_ERC20_ADDRESS, IERC20.abi, wallet1);
+    ACA = new ethers.Contract(ACA_ERC20_ADDRESS, IERC20.abi, wallet1);
+    AUSD = new ethers.Contract(AUSD_ERC20_ADDRESS, IERC20.abi, wallet1);
+    XBTC = new ethers.Contract(XBTC_ERC20_ADDRESS, IERC20.abi, wallet1);
+    LDOT = new ethers.Contract(LDOT_ERC20_ADDRESS, IERC20.abi, wallet1);
+    RENBTC = new ethers.Contract(RENBTC_ERC20_ADDRESS, IERC20.abi, wallet1);
   });
   
   after(async () => {
     provider.api.disconnect();
   });
   
-  it("Assigns initial balance", async () => {
-    //expect(await token.balanceOf(await wallet.getAddress())).to.equal(1000);
-    const wAddress = await wallet.getAddress();
-    console.log(wAddress);
-    const balance = await DOT.balanceOf(wAddress);
-    console.log(balance.toString());
+  it("Initial balances", async () => {
+    
+    const wAddress1 = await wallet1.getAddress();
+    console.log(wAddress1);
+    const dotBalance = await DOT.balanceOf(wAddress1);
+    const acaBalance = await ACA.balanceOf(wAddress1);
+    const ausdBalance = await AUSD.balanceOf(wAddress1);
+    const xbtcBalance = await XBTC.balanceOf(wAddress1);
+    const ldotBalance = await LDOT.balanceOf(wAddress1);
+    const renbtcBalance = await RENBTC.balanceOf(wAddress1);
+    console.log("DOT: ",dotBalance.toString()," ACA: ",acaBalance.toString());
+    console.log("AUSD: ",ausdBalance.toString()," XBTC: ",xbtcBalance.toString());
+    console.log("LDOT: ",ldotBalance.toString()," RENBTC: ",renbtcBalance.toString());
+    
+    const wAddress2 = await wallet2.getAddress();
+    console.log(wAddress2);
+    const dotBalance2 = await DOT.balanceOf(wAddress2);
+    const acaBalance2 = await ACA.balanceOf(wAddress2);
+    const ausdBalance2 = await AUSD.balanceOf(wAddress2);
+    const xbtcBalance2 = await XBTC.balanceOf(wAddress2);
+    const ldotBalance2 = await LDOT.balanceOf(wAddress2);
+    const renbtcBalance2 = await RENBTC.balanceOf(wAddress2);
+    console.log("DOT: ",dotBalance2.toString()," ACA: ",acaBalance2.toString());
+    console.log("AUSD: ",ausdBalance2.toString()," XBTC: ",xbtcBalance2.toString());
+    console.log("LDOT: ",ldotBalance2.toString()," RENBTC: ",renbtcBalance2.toString());
   });
 });
