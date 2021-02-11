dca929553fab5726a08d8191361aded8abfdda9e
Rafat Hussain <rafat.hsn@gmail.com>
2021-02-11 06:19:48 +0530

    cleanup on the test aisle

--- waffle/test/tokens.test.ts
+++ waffle/test/tokens.test.ts
@@ -58,26 +58,19 @@ const getWallets = async () => {
 describe("Tokens", () => {
   let wallet1: Signer;
   let wallet2: Signer;
-  let wallet3: Signer;
   let DOT: Contract;
   let ACA: Contract;
   let AUSD: Contract;
-  let XBTC: Contract;
-  let LDOT: Contract;
-  let RENBTC: Contract;
   let RTOK: Contract;
   let Exchange: Contract;
   let DOT2: Contract;
   let ACA2: Contract;
   
   before(async () => {
-    [wallet1, wallet2, wallet3] = await getWallets();
+    [wallet1, wallet2] = await getWallets();
     DOT = new ethers.Contract(DOT_ERC20_ADDRESS, IERC20.abi, wallet1);
     ACA = new ethers.Contract(ACA_ERC20_ADDRESS, IERC20.abi, wallet1);
     AUSD = new ethers.Contract(AUSD_ERC20_ADDRESS, IERC20.abi, wallet1);
-    XBTC = new ethers.Contract(XBTC_ERC20_ADDRESS, IERC20.abi, wallet1);
-    LDOT = new ethers.Contract(LDOT_ERC20_ADDRESS, IERC20.abi, wallet1);
-    RENBTC = new ethers.Contract(RENBTC_ERC20_ADDRESS, IERC20.abi, wallet1);
     const supply = BigNumber.from('1000000000000000000000000');
     RTOK = await deployContract(wallet1, RTOKABI, [supply] );
     Exchange = await deployContract(wallet1, DEX, [], {gasLimit: 40000000});
@@ -87,45 +80,6 @@ describe("Tokens", () => {
   
   after(async () => {
     provider.api.disconnect();
-  });
-  
-  it("Initial balances", async () => {
-    
-    const wAddress1 = await wallet1.getAddress();
-    console.log(wAddress1);
-    const dotBalance = await DOT.balanceOf(wAddress1);
-    const acaBalance = await ACA.balanceOf(wAddress1);
-    const ausdBalance = await AUSD.balanceOf(wAddress1);
-    const xbtcBalance = await XBTC.balanceOf(wAddress1);
-    const ldotBalance = await LDOT.balanceOf(wAddress1);
-    const renbtcBalance = await RENBTC.balanceOf(wAddress1);
-    console.log("DOT: ",dotBalance.toString()," ACA: ",acaBalance.toString());
-    console.log("AUSD: ",ausdBalance.toString()," XBTC: ",xbtcBalance.toString());
-    console.log("LDOT: ",ldotBalance.toString()," RENBTC: ",renbtcBalance.toString());
-    
-    const wAddress2 = await wallet2.getAddress();
-    console.log(wAddress2);
-    const dotBalance2 = await DOT.balanceOf(wAddress2);
-    const acaBalance2 = await ACA.balanceOf(wAddress2);
-    const ausdBalance2 = await AUSD.balanceOf(wAddress2);
-    const xbtcBalance2 = await XBTC.balanceOf(wAddress2);
-    const ldotBalance2 = await LDOT.balanceOf(wAddress2);
-    const renbtcBalance2 = await RENBTC.balanceOf(wAddress2);
-    console.log("DOT: ",dotBalance2.toString()," ACA: ",acaBalance2.toString());
-    console.log("AUSD: ",ausdBalance2.toString()," XBTC: ",xbtcBalance2.toString());
-    console.log("LDOT: ",ldotBalance2.toString()," RENBTC: ",renbtcBalance2.toString());
-    
-    const wAddress3 = await wallet3.getAddress();
-    console.log(wAddress3);
-    const dotBalance3 = await DOT.balanceOf(wAddress3);
-    const acaBalance3 = await ACA.balanceOf(wAddress3);
-    const ausdBalance3 = await AUSD.balanceOf(wAddress3);
-    const xbtcBalance3 = await XBTC.balanceOf(wAddress3);
-    const ldotBalance3 = await LDOT.balanceOf(wAddress3);
-    const renbtcBalance3 = await RENBTC.balanceOf(wAddress3);
-    console.log("DOT: ",dotBalance3.toString()," ACA: ",acaBalance3.toString());
-    console.log("AUSD: ",ausdBalance3.toString()," XBTC: ",xbtcBalance3.toString());
-    console.log("LDOT: ",ldotBalance3.toString()," RENBTC: ",renbtcBalance3.toString());
   });
 
   it("Custom Token RTOK", async () => {
@@ -380,14 +334,12 @@ describe("Tokens", () => {
 
     txTrade1 = await Exchange.tradeOffer(ACA_ERC20_ADDRESS,sellAmount3,DOT_ERC20_ADDRESS,buyAmount3);
 
-    let offerSize = await Exchange.getOfferSize(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
-
+    let offerSize = await Exchange.getOfferSize(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);    
     let offerId = await Exchange.getBestOffer(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
-
+    
     expect(offerSize.toString()).to.equal('3');
-    expect(offerId.toString()).to.equal('1');
 
-    // Account 1 checks th available orders and cancels them all
+    // Account 1 checks the available orders and cancels them all
 
     offerSize = await Exchange.getOfferSize(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
 
@@ -402,9 +354,6 @@ describe("Tokens", () => {
 
     offerSize = await Exchange.getOfferSize(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
 
-    offerId = await Exchange.getBestOffer(ACA_ERC20_ADDRESS,DOT_ERC20_ADDRESS);
-
     expect(offerSize.toString()).to.equal('0');
-    expect(offerId.toString()).to.equal('0');
   })
 });
